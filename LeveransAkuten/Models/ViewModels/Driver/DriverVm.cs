ef0225e2ee6ef4b6f819c7b82de16a010d8e2475
OndiSkogen <fiskkneipp@gmail.com>
2018-12-29 16:52:39 +0100

    Added DriverDetails.css, Driver/Details.cshtml and DriverVM

---
+++ LeveransAkuten/Models/ViewModels/Driver/DriverVm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeveransAkuten.Models.ViewModels.Driver
+{
+    public class DriverVm
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string ImageUrl { get; set; }
+        public string StreetAdress { get; set; }
+        public string ZipCode { get; set; }
+        public string City { get; set; }
+        public string Description { get; set; }
+        public bool A { get; set; }
+        public bool B { get; set; }
+        public bool C { get; set; }
+        public bool D { get; set; }
+        public bool CE { get; set; }
+    }
+}
