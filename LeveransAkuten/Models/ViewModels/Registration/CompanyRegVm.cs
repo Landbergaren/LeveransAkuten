6e69a649c465410d4df7a301c76dec035f16ee90
Tomas <Landbergaren@gmail.com>
2018-12-29 17:17:37 +0100

    importing vm to view and formating ui

--- LeveransAkuten/Models/ViewModels/Registration/CompanyRegVm.cs
+++ LeveransAkuten/Models/ViewModels/Registration/CompanyRegVm.cs
@@ -7,7 +7,7 @@ namespace LeveransAkuten.Models.ViewModels.Registration
 {
     public class CompanyRegVm
     {
-        public string UserName { get; set; }
+        public string Username { get; set; }
         public string Password { get; set; }
         public string CompanyName { get; set; }
         public string PhoneNumber { get; set; }
