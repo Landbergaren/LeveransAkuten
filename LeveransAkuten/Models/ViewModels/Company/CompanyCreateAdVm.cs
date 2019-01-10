fc0897f27d630c83c2004ecf81b047d099fbb213
Tomas <Landbergaren@gmail.com>
2019-01-10 11:46:27 +0100

    Unique viewmodel for CreateAd

---
+++ LeveransAkuten/Models/ViewModels/Company/CompanyCreateAdVm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LeveransAkuten.Models.ViewModels.Company
+{
+    public class CompanyCreateAdVm
+    {
+
+        [Required]
+        [Display(Name = "Rubrik")]
+        public string Header { get; set; }
+
+        [Required]
+        [Display(Name = "Beskrivning")]
+        public string Description { get; set; }
+
+        [Required]
+        [Display(Name = "Startdatum")]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        [Display(Name = "Slutdatum")]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "A")]
+        public bool Arequired { get; set; }
+        [Display(Name = "B")]
+        public bool Brequired { get; set; }
+        [Display(Name = "C")]
+        public bool Crequired { get; set; }
+        [Display(Name = "D")]
+        public bool Drequired { get; set; }
+        [Display(Name = "CE")]
+        public bool Cerequired { get; set; }
+    }
+}
